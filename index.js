be8a630738955e8c4fafd0248273dd4465051552
Ankon Chowdhury <ankonchy1@gmail.com>
2025-08-31 23:57:57 +0600

    user schema and model created

--- index.js
+++ index.js
@@ -5,11 +5,52 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 9000;
 
+app.use(express.json());
+
+app.post("/products", async (req, res) => {
+  try {
+    // const title = req.body.title;
+    // const price = req.body.price;
+    // const description = req.body.description;
+
+    const newProduct = new Product({
+      title: req.body.title,
+      price: req.body.price,
+      description: req.body.description,
+    });
+
+    const productData = await newProduct.save();
+    res.status(201).send(productData);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ri84s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ri84s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ri84s.mongodb.net/Red-donor?retryWrites=true&w=majority&appName=Cluster0`;
+
+//create product schema
+const productsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//create product model
+const Product = mongoose.model("Products", productsSchema);
 const connectDB = async () => {
   try {
     await mongoose.connect(uri);
---
+++ models/Users.js
@@ -0,0 +1,28 @@
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin", "donor", "volunteer"],
+      default: "donor",
+    },
+    avatar: { type: String },
+    bloodGroup: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+    },
+    district: {
+      type: String,
+    },
+    upazila: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
